e074f71b658459bf88ea233c671f2d7f84bbd3e6
Jonathan Vanasse <jonvanasse@gmail.com>
2021-01-17 14:20:53 -0600

    Seed Script is Done; getting ready to work on testing and front end

--- database/index.js
+++ database/index.js
@@ -1,44 +1,95 @@
+// REQUIRES
+
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017', {useNewUrlParser: true, useUnifiedTopology: true});
+const faker = require('faker');
+
+// DATABASE INITIALIZATION
+
+mongoose.connect('mongodb://localhost/attractions', {useNewUrlParser: true, useUnifiedTopology: true});
+
 const db = mongoose.connection;
 
+db.on('error', console.error.bind(console, 'connection error: '));
+db.once('open', () => {
+  console.log(`we're connected!`)
+});
+
+db.onLoad = () => {
+  console.log('Loaded')
+}
+
+// MODEL
+
 const attractionSchema = new mongoose.Schema({
-  attractionId: Number,
+  _id: mongoose.Schema.Types.ObjectId,
   attractionName: String,
   reviewCount: Number,
   attractionRank: Array,
   category: String,
   tourCount: Number,
   greaterArea: String,
-  reviews: [reviewSchema], // array of reviewIds (SEE BELOW)
+  reviews: Array
+  /*       of these objects:
+    {
+      createdAt: Date,
+      rating: Number,
+      tagline: String,
+      body: String,
+      reviewer: {
+        username: String,
+        iconURL: String
+      },
+      helpful: Number,
+      photos: [Array]
+    } */
 });
+const Attraction = new mongoose.model('Attraction', attractionSchema);
 
-const reviewSchema = new mongoose.Schema({
-  reviewId: Number,
-  createdAt: Date,
-  attractionId: Number,
-  rating: Number,
-  tagline: String,
-  body: String,
-  user: [userSchema],
-  iconUrl: String,
-  helpful: Number,
-  photos: Array
-});
+const seedDatabase = () => {
+  const attraction = {
+    attractionName: 'Punaauia Beach',
+    reviewCount: faker.random.number({max: 70, min: 13}),
+    attractionRank: [faker.random.number(3), faker.random.number({max: 11, min: 4})],
+    category: 'Beaches',
+    tourCount: faker.random.number(15),
+    greaterArea: 'Tahiti',
+    reviews: []
+  }
+  for (let i = 0; i < attraction.reviewCount; i++) {
+    // generating the body text for this review
+    let body = []
+    for (let j = 0; j < faker.random.number(3); j++) {
+      body.push(faker.lorem.sentences(faker.random.number(3)));
+    }
+    // generating the photo array for this review
+    let photos = []
+    let options = ['nature', 'animals', 'city', 'food']
+    for (let j = 0; j < faker.random.number(7); j++) {
+      let option = options[faker.random.number({max: 3, min: 0})];
+      photos.push(faker.image.imageUrl(640, 480, option, true, true))
+    }
+    // create the review with the nested docs
+    let review = {
+      createdAt: faker.date.past(faker.random.number({max:9, min: 0})),
+      rating: faker.random.number({max: 5, min: 1, }),
+      tagline: faker.lorem.sentence(),
+      body: body.join('\n'),
+      reviewer: {
+        username: faker.internet.userName(),
+        iconURL: faker.image.people(150, 150),
+      },
+      helpful: faker.random.number(80),
+      photos: [...photos]
+    }
 
-const userSchema = new mongoose.Schema({
-  userId: Number,
-  userName: String,
-  iconURL: String
-})
+    attraction.reviews.push(review)
+  }
 
-const attraction = new mongoose.model(attractionSchema);
+  Attraction.findOneAndUpdate({_id: attraction._id}, attraction, {j: true, new: true, upsert: true}).catch(err => console.log(err));
 
-db.on('error', console.error.bind(console, 'connection error: '));
-db.once(open, () => {
-  console.log(`we're connected!`)
-});
+  return attraction;
+}
 
-// db.searchAttractions = db.query();
+const attraction = seedDatabase();
 
-module.exports.db = db;
+module.exports.attraction = attraction;
--- server/index.js
+++ server/index.js
@@ -2,13 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const path = require('path');
-const db = require('../database');
+const cors = require('cors');
 
 const PORT = 3000 || process.env.PORT;
 const STATIC_DIR = path.resolve(__dirname, '..', 'client', 'dist');
+const attraction = require(path.resolve(__dirname, '..', 'database', 'index.js'))
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(morgan('dev'));
@@ -20,8 +22,8 @@ app.use((req, res, next) => {
 })
 
 app.get('/', (req, res) => {
-  console.log(`${req.body} is the request body`);
-  res.status(200).send('Good job!');
+  console.log(`${req.body} is the request body on the req for ${req.url}`);
+  res.status(200).send(JSON.stringify(attraction));
 })
 
 app.listen(PORT, () => {
